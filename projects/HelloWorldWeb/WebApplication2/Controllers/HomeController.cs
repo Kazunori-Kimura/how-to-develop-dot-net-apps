bc04b1c6b6e638e0c3d7595cfd107d5b10d4e166
kazunori.kimura <kazunori.kimura.js@gmail.com>
2015-11-02 16:00:28 +0900

    add image & project files

---
+++ projects/HelloWorldWeb/WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View(new UserModel());
+        }
+
+        /// <summary>
+        /// 名前をpostされたときの処理
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult Index([Bind(Include = "Name")] UserModel user)
+        {
+            return RedirectToAction("Hello", user);
+        }
+
+        /// <summary>
+        /// あいさつページの表示
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        public ActionResult Hello(UserModel user)
+        {
+            if (string.IsNullOrEmpty(user.Name))
+            {
+                return RedirectToAction("Index");
+            }
+            return View(user);
+        }
+    }
+}
---
+++ projects/HelloWorldWeb/WebApplication2/Models/UserModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class UserModel
+    {
+        /// <summary>
+        /// 名前
+        /// </summary>
+        [DisplayName("お名前")]
+        public string Name { get; set; }
+    }
+}
